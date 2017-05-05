55cbcd7e4b4bf55f376d40d918f023bd5306136d
Vodka_Crew <manbrk@users.noreply.github.com>
2017-05-05 20:01:30 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular TODO App';
+  tasks = [
+      {
+          task: 'buckshot',
+          state: false
+      },
+      {
+          task: '9mm',
+          state: true
+      },
+      {
+          task: '.50',
+          state: false
+      }
+      ];
+
+    addTask(task) {
+        if (task) {
+            this.tasks.push({
+            task: task,
+            state: false
+            });
+        }
+    };
+
+    toggleSwitch(item) {
+        item.state = !item.state;
+    };
+
+    remove(index) {
+        this.tasks.splice(index, 1);
+    };
 }
